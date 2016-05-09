8e6fa221e9d96fdfa6a492cb4dc71375e62e8f87
GuilhermeCFernandes <guilherme.correa.fernandes255@gmail.com>
2016-05-09 08:27:58 -0300

    Versao final do projeto anterior

--- Bozo/src/Placar.java
+++ Bozo/src/Placar.java
@@ -1,97 +1,133 @@
+/**
+ * Representa o placar do jogo, cuida da pontuaçao
+ * @author Tiago Esperança Triques
+ * @author Guilherme Correa Fernandes
+ */
 import java.util.Arrays;
 
 public class Placar {
-	
-	int score;
-	int [][] vetor = new int [10][5];
-	int [] matrizDePontos = new int [10];
-	
-	public Placar(){
-//		for (int[] v : vetor)
-//			v = null;
-		Arrays.fill(matrizDePontos, 0);
-	}
+    
+    int score;
+    int [][] configDados = new int [10][5];
+    int [] pontos = new int [10];
+    int [] pontosDasPosicoes = new int[]{1, 2, 3, 4, 5, 6, 15, 20, 30 ,40};
+    
+    public Placar(){
+        Arrays.fill(pontos, -1);
+    }
 
-	private int sumAux (int [] dados, int h){
-		int resultado = 0;
-		for (int v: dados)
-			if(v == h)
-				resultado += v;
-		return resultado;
-	}
-	
-	private int counter (int [] dados, int h){
-		return sumAux(dados, h) / h;
-	}
-	
-	private int [] countAll (int [] dados){
-		int [] result = new int [6];
-		for(int i = 0; i < 5; i ++){
-			result[i] = counter(dados, i + 1);
-		}
-		return result;
-	}
-	
-	boolean isValid(int posicao, int [] dados){
-		
-		int [] aux = new int [6];
-		aux = countAll(dados);
-		if (matrizDePontos[posicao] != 0)
-			return false;
-		//parte facil
-		if(posicao >= 0 && posicao <= 5 && sumAux(dados, posicao + 1) != 0){
-			matrizDePontos[posicao] += sumAux(dados, posicao + 1);
-			return true;
-		}
-		//full house/hand
-		if(posicao == 6){
-			boolean flag2 = false, flag3 = false;
-			for(int v: aux){
-				if(v == 2)
-					flag2 = true;
-				if(v == 3)
-					flag3 = true;
-			}
-			if(flag2 && flag3){
-				matrizDePontos[posicao] += 15;
-				return true;
-			}
-		}
-		if(posicao == 7){
-			if(Arrays.equals(aux, new int[]{1,1,1,1,1,0}) || Arrays.equals(aux, new int[]{0,1,1,1,1,1})){
-				matrizDePontos[posicao] += 20;
-				return true;
-			}
-		}
-		if(posicao == 8){
-			for(int v: aux)
-				if(v == 4){
-					matrizDePontos[posicao] += 30;
-					return true;
-				}
-		}
-		if(posicao == 9){
-			for(int v: aux)
-				if(v == 5){
-					matrizDePontos[posicao] += 40;
-					return true;
-				}
-		}
-		
-		return false;
-	}
-	
-	public void add (int posicao, int [] dados){
-		if(isValid(posicao, dados))
-			vetor[posicao] = dados;
-	}
-	
-	public int getScore(){
-		return score;
-	}
-	
-	@Override
-		public String toString(){
-		return String.format("(%d)\t|(%d)\t|(%d)\n--------------------------\n(%d)\t|(%d)\t|(%d)\n--------------------------\n(%d)\t|(%d)\t|(%d)\n--------------------------\n\t|(%d)\t|\n        +-------+\n",matrizDePontos[0], matrizDePontos[6], matrizDePontos[3], matrizDePontos[1], matrizDePontos[7], matrizDePontos[4], matrizDePontos[2], matrizDePontos[8], matrizDePontos[5], matrizDePontos[9]);
-	}
+    private int sumAux (int [] dados, int h){
+        int resultado = 0;
+        for (int v: dados)
+            if(v == h)
+                resultado += v;
+        return resultado;
+    }
+    
+    private int counter (int [] dados, int h){
+        return sumAux(dados, h) / h;
+    }
+    
+    private int [] countAll (int [] dados){
+        int [] result = new int [6];
+        for(int i = 0; i < 5; i ++){
+            result[i] = counter(dados, i + 1);
+        }
+        return result;
+    }
+   
+    private boolean isValid(int posicao, int [] dados){
+        if(posicao >= 0 && posicao < 10 && pontos[posicao] != -1) {
+            return false;
+        }
+
+        int [] aux = new int [5];
+        aux = countAll(dados);
+        
+        if(posicao >= 0 && posicao <= 5 && sumAux(dados, posicao + 1) != 0){
+            score += sumAux(dados, posicao + 1);
+            pontos[posicao] = sumAux(dados, posicao + 1);
+            return true;
+        }
+       
+        if(posicao == 6){
+            boolean dupla = false, trio = false;
+            for(int v: aux){
+                if(v == 2)
+                    dupla = true;
+                if(v == 3)
+                    trio = true;
+            }
+            if(dupla && trio){
+                score += 15;
+                pontos[posicao] = 15;
+                return true;
+            }
+        }
+
+        if(posicao == 7){
+            if(Arrays.equals(aux, new int[]{1,1,1,1,1,0}) || Arrays.equals(aux, new int[]{0,1,1,1,1,1})){
+                score += 20;
+                pontos[posicao] = 20;
+                return true;
+            }
+        }
+        if(posicao == 8){
+            for(int v: aux)
+                if(v == 4){
+                    score += 30;
+                    pontos[posicao] = 30;
+                    return true;
+                }
+        }
+
+        if(posicao == 9){
+            for(int v: aux)
+                if(v == 5){
+                    score += 40;
+                    pontos[posicao] = 40;
+                    return true;
+                }
+        }
+        
+        return false;
+    }
+	/**
+	 * Adiciona uma combinacao de dados, se for valida, para uma posicao do placar.
+	 * Cada posicao so pode ser usada uma vez
+	 * @param posicao Posicao do placar para armazenar os dados
+	 * @param dados Sequencia de dados
+	 */
+    public void add (int posicao, int [] dados){
+        if(isValid(posicao, dados)) {
+            configDados[posicao] = dados;
+        }
+    }
+    /**
+     * Retorna a pontuaçao atual
+     * @return Pontuacao
+     */
+    public int getScore(){
+        return score;
+    }
+    
+    private String posicaoPlacar(int n){
+    	String s = new String();
+    	if(pontos[n] == -1){
+    		s += '(';
+    		s += pontosDasPosicoes[n];
+    		s += ')';
+    	}
+    	else
+    		s += pontos[n];
+    	return s;
+    }
+    
+    /**
+     * Representa o placar na forma de uma string, mostrando as posicoes livre(com os valores delas), e as ja ocupadas(com os pontos obtidos em cada uma)
+     */
+    @Override
+        public String toString(){
+        return String.format("%s\t|%s\t|%s\n--------------------------\n%s\t|%s\t|%s\n--------------------------\n%s\t|%s\t|%s\n--------------------------\n\t|%s\t|\n        +-------+\n",posicaoPlacar(0), posicaoPlacar(6), posicaoPlacar(3), posicaoPlacar(1),posicaoPlacar(7), posicaoPlacar(4), posicaoPlacar(2), posicaoPlacar(8), posicaoPlacar(5), posicaoPlacar(9));
+    }
 }
